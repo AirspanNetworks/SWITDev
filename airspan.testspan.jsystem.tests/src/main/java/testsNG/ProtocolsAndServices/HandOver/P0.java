f8caaab28189321e7b58194a515f7cd39c10793e
ggrunwald <ggrunwald@ASIL-GGRUNWALD.airspan.com>
2018-06-26 12:17:25 +0300

    Remove ue show link

--- airspan.testspan.jsystem.tests/src/main/java/testsNG/ProtocolsAndServices/HandOver/P0.java
+++ airspan.testspan.jsystem.tests/src/main/java/testsNG/ProtocolsAndServices/HandOver/P0.java
@@ -1143,7 +1143,6 @@ public class P0 extends TestspanTest {
 	public void commands() {
 
 		ArrayList<String> commands = new ArrayList<>();
-		commands.add("ue show link");
 		commands.add("ue show rate");
 		commands.add("rlc show amdlstats");
 		commands.add("system show error");

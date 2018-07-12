caed30dc0e28a17161c62128a98d5d462420d7d0
ggrunwald <ggrunwald@ASIL-GGRUNWALD.airspan.com>
2018-07-12 11:31:29 +0300

    fix printing
    Former-commit-id: 6dfb91fd1039d7ee2c5790e36c2c707be9ff4354

--- airspan.testspan.jsystem.sysobj/src/main/java/Utils/LteThroughputCalculator.java
+++ airspan.testspan.jsystem.sysobj/src/main/java/Utils/LteThroughputCalculator.java
@@ -347,8 +347,8 @@ public class LteThroughputCalculator {
 	        
 			Double dl = (Double)findDL.evaluate(doc, XPathConstants.NUMBER);
 			Double ul = (Double)findUL.evaluate(doc, XPathConstants.NUMBER);
-			GeneralUtils.printToConsole("Value of element "+findDL+": "+dl);
-			GeneralUtils.printToConsole("Value of element "+findUL+": "+ul);
+			GeneralUtils.printToConsole("Value of element "+dlPassCriteriaXmlPath+": "+dl);
+			GeneralUtils.printToConsole("Value of element "+ulPassCriteriaXmlPath+": "+ul);
 			if(dl != null && ul != null && dl > 0 && ul > 0){
 				dl_ul = dl + "_" + ul;
 			}

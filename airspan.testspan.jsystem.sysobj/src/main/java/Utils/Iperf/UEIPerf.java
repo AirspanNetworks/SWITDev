2d56854f0b33d864957e0e6283800696bb907df2
ggrunwald <ggrunwald@ASIL-GGRUNWALD.airspan.com>
2018-08-01 08:49:38 +0300

    Add delay
    Former-commit-id: 2f51cae89e8f2fd3957c55962a2e5e3d45c8bf1c

--- airspan.testspan.jsystem.sysobj/src/main/java/Utils/Iperf/UEIPerf.java
+++ airspan.testspan.jsystem.sysobj/src/main/java/Utils/Iperf/UEIPerf.java
@@ -154,7 +154,9 @@ public class UEIPerf implements Runnable {
 		/**It is critical to start to listen before transmitting traffic for TCP Packets!!!
 		   without it - it won't work! **/
 		startULListener();
+		GeneralUtils.unSafeSleep(1000);
 		runTrafficULClient();
+		GeneralUtils.unSafeSleep(1000);
 	}
 
 	protected void runTrafficULClient() {
@@ -186,7 +188,9 @@ public class UEIPerf implements Runnable {
 		/**It is critical to start to listen before transmitting traffic for TCP Packets!!!
 		   without it - it won't work! **/
 		startDLListener();
+		GeneralUtils.unSafeSleep(1000);
 		runTrafficDLClient();
+		GeneralUtils.unSafeSleep(1000);
 	}
 
 	protected void runTrafficDLClient() {

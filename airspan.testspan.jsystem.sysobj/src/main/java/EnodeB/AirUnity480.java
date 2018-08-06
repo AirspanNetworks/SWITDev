445b44ea9b10f1be08e77a7f09eeda85c4471cba
ggrunwald <ggrunwald@ASIL-GGRUNWALD.airspan.com>
2018-08-06 16:21:31 +0300

    Add constructor
    Former-commit-id: 89056e3e0278774eba7c21515d28f25bd99d86aa

--- airspan.testspan.jsystem.sysobj/src/main/java/EnodeB/AirUnity480.java
+++ airspan.testspan.jsystem.sysobj/src/main/java/EnodeB/AirUnity480.java
@@ -1,9 +1,14 @@
 package EnodeB;
 
 public class AirUnity480 extends AirVelocity{
+	
+	public AirUnity480() {
+		super();
+		setSWTypeInstance(22);
+	}
+	
 	@Override
 	public void init() throws Exception {
 		super.init();
-		setSWTypeInstance(22);
 	}
 }

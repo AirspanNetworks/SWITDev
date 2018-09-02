9ec586c57096f79403aab4ad2496b12ac5d51c4f
ggrunwald <ggrunwald@ASIL-GGRUNWALD.airspan.com>
2018-09-02 15:47:11 +0300

    try with no sampling during running
    Former-commit-id: 8c74aabb5fa8aaacbd30179efaadd4b795bac860

--- airspan.testspan.jsystem.tests/src/main/java/testsNG/Actions/TrafficSampler.java
+++ airspan.testspan.jsystem.tests/src/main/java/testsNG/Actions/TrafficSampler.java
@@ -35,8 +35,8 @@ public class TrafficSampler implements Runnable{
 	public static Reporter report = ListenerstManager.getInstance();
 	
 	public void start(){
-		runnableThread = new Thread(this);
-		runnableThread.start();			
+		//runnableThread = new Thread(this);
+		//runnableThread.start();			
 	}
 	
 	public void getStatistics(){

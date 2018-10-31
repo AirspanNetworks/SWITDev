53d00753bd6c532469c3717d0247e46bfc7c85fe
ggrunwald <ggrunwald@ASIL-GGRUNWALD.airspan.com>
2018-10-31 10:57:21 +0200

    more sleep time
    Former-commit-id: bccf7ae15546111cbe225713ea286ae9b68b2a6d

--- airspan.testspan.jsystem.tests/src/main/java/Action/BasicAction/BasicAction.java
+++ airspan.testspan.jsystem.tests/src/main/java/Action/BasicAction/BasicAction.java
@@ -148,7 +148,7 @@ public class BasicAction extends Action {
 				GeneralUtils.unSafeSleep(1000);
 				report.report("Response for "+cmd+":"+output);
 			}
-			GeneralUtils.unSafeSleep(5000);
+			GeneralUtils.unSafeSleep(20000);
 			ssh.disconnect();
 		}else{
 			report.report("Failed to connect to device",Reporter.FAIL);

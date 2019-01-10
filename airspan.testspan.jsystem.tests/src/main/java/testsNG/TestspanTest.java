85a181506113a4abe5bea18cd0524e990e2009a8
Dor Shalom <dshalom@airspan.com>
2019-01-10 16:59:50 +0200

    Create db comperison flag AUT-1408
    Former-commit-id: bd2cb32729058a0127f0c076695d6d0c004b200b

--- airspan.testspan.jsystem.tests/src/main/java/testsNG/TestspanTest.java
+++ airspan.testspan.jsystem.tests/src/main/java/testsNG/TestspanTest.java
@@ -37,6 +37,7 @@ import Utils.Snmp.MibReader;
 import Utils.WatchDog.CommandMemoryCPU;
 import Utils.WatchDog.CommandWatchInService;
 import Utils.WatchDog.WatchDogManager;
+import jsystem.framework.ParameterProperties;
 import jsystem.framework.report.ListenerstManager;
 import jsystem.framework.report.Reporter;
 import junit.framework.SystemTestCase4;
@@ -102,7 +103,7 @@ public class TestspanTest extends SystemTestCase4 {
      */
     private static HashMap<String, Integer> unexpectedRebootHash = null;
     protected WatchDogManager wd;
-
+    private boolean performDbComperison;
     /**
      * Initialize function
      *
@@ -710,13 +711,15 @@ public class TestspanTest extends SystemTestCase4 {
      * @param eNodeB - eNodeB
      */
     private void getAndCompareDBFiles(EnodeB eNodeB) {
+    	if(performDbComperison){
         ArrayList<String> fileNameList = filesFromDBPerNode.get(eNodeB.getName());
-        if ((fileNameList != null) && (!fileNameList.isEmpty())) {
-            report.report("getDBFiles starts now:");
-            getDBFiles(eNodeB, afterTest);
-            report.report("compareDBs starts now:");
-            compareDBs(eNodeB);
-        }
+	        if ((fileNameList != null) && (!fileNameList.isEmpty())) {
+	            report.report("getDBFiles starts now:");
+	            getDBFiles(eNodeB, afterTest);
+	            report.report("compareDBs starts now:");
+	            compareDBs(eNodeB);
+	        }
+    	}
     }
 
     /**
@@ -920,28 +923,29 @@ public class TestspanTest extends SystemTestCase4 {
      * @author sshahaf
      */
     private Boolean getDBFiles(EnodeB eNodeB, String beforeTestExtention) {
-        initDBFilesFeature(eNodeB, beforeTestExtention);
-
-        dumpDBAndCreateLocalDirectory(eNodeB, eNodeB.getName() + beforeTestExtention);
-
-        String filesNames = getFileNamesFromNode(eNodeB);
-        if (filesNames == null) {
-            report.report("could not get files from node!");
-            return false;
+        if(performDbComperison){
+	    	initDBFilesFeature(eNodeB, beforeTestExtention);
+	
+	        dumpDBAndCreateLocalDirectory(eNodeB, eNodeB.getName() + beforeTestExtention);
+	
+	        String filesNames = getFileNamesFromNode(eNodeB);
+	        if (filesNames == null) {
+	            report.report("could not get files from node!");
+	            return false;
+	        }
+	
+	        ArrayList<String> filesFromDB = filterFileNames(filesNames, ".*\\.xml", "Stat", "stat", "smonMeasurement.xml");
+	        if (filesFromDB.isEmpty()) {
+	            return false;
+	        }
+	        GeneralUtils.printToConsole("add files names to map - node : " + eNodeB.getName());
+	        filesFromDBPerNode.put(eNodeB.getName(), filesFromDB);
+	
+	        if (!transferFilesToLocalComputer(scpCli, filesFromDB, eNodeB, beforeTestExtention)) {
+	            report.report("Could not get files from Node");
+	            return false;
+	        }
         }
-
-        ArrayList<String> filesFromDB = filterFileNames(filesNames, ".*\\.xml", "Stat", "stat", "smonMeasurement.xml");
-        if (filesFromDB.isEmpty()) {
-            return false;
-        }
-        GeneralUtils.printToConsole("add files names to map - node : " + eNodeB.getName());
-        filesFromDBPerNode.put(eNodeB.getName(), filesFromDB);
-
-        if (!transferFilesToLocalComputer(scpCli, filesFromDB, eNodeB, beforeTestExtention)) {
-            report.report("Could not get files from Node");
-            return false;
-        }
-
         return true;
     }
 
@@ -1178,4 +1182,8 @@ public class TestspanTest extends SystemTestCase4 {
         }
     }
 
+    @ParameterProperties(description = "Set Perform Db Comperison")
+	public void setPerformDbComperison(boolean performDbComperison) {
+		this.performDbComperison = performDbComperison;
+	}
 }

75d60f6e83658a15d05191d98f64d4f67b9c3801
Dor Shalom <dshalom@airspan.com>
2019-01-31 15:21:12 +0200

    Make AirUnity480 extend EnodeBWithDonor AUT-1450
    Former-commit-id: 5321cc50fff480cd3bf09da5ddf6bb83f5e707df

--- airspan.testspan.jsystem.sysobj/src/main/java/EnodeB/AirUnity480.java
+++ airspan.testspan.jsystem.sysobj/src/main/java/EnodeB/AirUnity480.java
@@ -1,8 +1,12 @@
 package EnodeB;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import EnodeB.Components.EnodeBComponent;
 import Netspan.API.Enums.ImageType;
 
-public class AirUnity480 extends AirVelocity{
+public class AirUnity480 extends EnodeBWithDonor{
 	
 	public AirUnity480() {
 		super();
@@ -17,5 +21,20 @@ public class AirUnity480 extends AirVelocity{
 	@Override
 	public ImageType getImageType() {
 		return ImageType.COMBINED_LTE_RELAY;
+	}
+
+	@Override
+	public String getRelayRunningVersion() {
+		String response = XLP.shell("ls /bs/relay/ue/altair");
+		String regexPattern = 	"(FL(_\\d+)+)";
+		Pattern pattern = Pattern.compile(regexPattern);
+		Matcher matcher = pattern.matcher(response);
+		String altairVersion =  matcher.find() ? matcher.group(1) : "";
+		if(altairVersion == ""){
+			response = XLP.sendCommandsOnSession(XLP.getSerialSession().getName(), EnodeBComponent.SHELL_PROMPT, "ls /bs/relay/ue/altair", "");
+			matcher = pattern.matcher(response);
+			altairVersion =  matcher.find() ? matcher.group(1) : "";
+		}
+		return altairVersion;
 	}
 }

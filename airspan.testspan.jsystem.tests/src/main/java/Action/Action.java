d4d583ea9b9cc11573b798de3d9658a16aa04717
doguz <doguz@airspan.com>
2019-03-04 15:15:41 +0200

    Revert "Add reason report ability to Action base class In fact - copy proper functions and properties from testNG.Testspan.java"
    This reverts commit 0fa4103ee247b37743afab78745dc46493f874ce [formerly 3faabc87bfc35b0c8b1665bbad92528b65019527].
    
    
    Former-commit-id: 67ee5647f9c6b3ea34a840a7427eb3c91e04ef9d

--- airspan.testspan.jsystem.tests/src/main/java/Action/Action.java
+++ airspan.testspan.jsystem.tests/src/main/java/Action/Action.java
@@ -3,13 +3,10 @@ package Action;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.util.HashMap;
 
-import org.apache.commons.lang3.StringUtils;
 import org.junit.After;
 import org.junit.Before;
 
-import Utils.ScenarioUtils;
 import jsystem.framework.report.Reporter;
 import junit.framework.SystemTestCase4;
 
@@ -20,131 +17,22 @@ public class Action extends SystemTestCase4 {
 	public static final String PATH_TO_POM_PROPERTIES = System.getProperty("user.dir") + File.separator + "target"
 			+ File.separator + "maven-archiver" + File.separator + DEFAULT_SOURCE_POM_PROPERTIES_FILE_NAME;
 	protected String myVersion = "";
-	/**
-     * The Constant SYSOBJ_STRING_DELIMITER.
-     */
-    protected String reason = StringUtils.EMPTY;
-    private StringBuilder coreFilesPath;
-    private boolean isCoreOccurDuringTest;
-    private static HashMap<String, Integer> testStats;
-    
-    
+	
 	protected enum Comparison {
 		EQUAL_TO, NOT_EQUAL_TO, BIGGER_THAN, SMALLER_THAN
 	}
 	
 	@Before
 	public void init() {
-		testStats = new HashMap<String, Integer>();
 		myVersion = getVersion();
 		report.setContainerProperties(0, "Version", myVersion);
 	}
 
 	@After
 	public void end() {
-		uploadParamsToReporter();
 		setLastStatusRetParam();
 	}
-	
-	/**
-     * Print Fail Reason To Prop File
-     */
-    private void printFailReasonToPropFile() {
-        if (!reason.isEmpty()) {
-            report.addProperty("failureReason", reason);
-            report.report("Fail reason: " + reason);
-        }
-    }
 
-    /**
-     * Set this.reason (of failed test) when isCoreOccurDuringTest=true
-     *
-     * @param isCoreOccurDuringTest - isCoreOccurDuringTest bool
-     */
-    private void setReasonWhenCoreOccur(boolean isCoreOccurDuringTest) {
-        if (isCoreOccurDuringTest) {
-            if (!reason.isEmpty()) {
-                reason += "<br>Core occurred during test.";
-            } else {
-                reason = "Core occurred during test.";
-            }
-        }
-    }
-    
-    /**
-     * Print Core Files To Prop File
-     *
-     * @param coreFilesPath - coreFilesPath
-     */
-    private void printCoreFilesToPropFile(StringBuilder coreFilesPath) {
-        if (!coreFilesPath.toString().isEmpty()) {
-            report.addProperty("CoreFiles", coreFilesPath.toString());
-        }
-    }
-    
-    /**
-     * Get Log Counter String Pattern, to be printed.
-     *
-     * @param key   - key
-     * @param entry - entry
-     * @return - Log Counter String Pattern
-     */
-    private String getLogCounterStringPattern(String key, HashMap<String, Integer> entry) {
-        return key + "," + entry.get(key) + ScenarioUtils.SYSOBJ_STRING_DELIMITER;
-    }
-    
-    /**
-     * GetLogCounter in order to print it and add it to prop file
-     */
-    private String getLogCounter(HashMap<String, Integer> hashMapStatus) {
-        if (hashMapStatus != null && !hashMapStatus.isEmpty()) {
-            StringBuilder logCounter = new StringBuilder(StringUtils.EMPTY);
-            for (String key : hashMapStatus.keySet()) {
-                logCounter.append(getLogCounterStringPattern(key, hashMapStatus));
-            }
-            return logCounter.toString();
-        }
-        return StringUtils.EMPTY;
-    }
-    
-    /**
-     * Add Log Counter To Prop File
-     */
-    private void addLogTestCounterToPropFile() {
-        String testStatsLogCounter = getLogCounter(testStats);
-        if (!testStatsLogCounter.isEmpty()) {
-            report.addProperty("LogCounter", testStatsLogCounter);
-        }
-    }
-    
-//    /**
-//     * Print Log Scenario Counter To Prop File
-//     */
-//    private void printLogScenarioCounterToPropFile() {
-//        String scenarioStatsLogCounter = getLogCounter(scenarioStats);
-//        if (!scenarioStatsLogCounter.isEmpty()) {
-//            report.setContainerProperties(0, "LogCounter", scenarioStatsLogCounter);
-//        }
-//    }
-    
-    /**
-     * Add Log Test Counter, and print Log Scenario Counter To Prop File
-     */
-    private void handleLogCounter() {
-        addLogTestCounterToPropFile();
-//        printLogScenarioCounterToPropFile();
-    }
-    
-    /**
-     * Upload Params To Reporter
-     */
-    private void uploadParamsToReporter() {
-        setReasonWhenCoreOccur(isCoreOccurDuringTest);
-        printFailReasonToPropFile();
-        printCoreFilesToPropFile(coreFilesPath);
-        handleLogCounter();
-    }
-	
 	private void setLastStatusRetParam() {
 		if (this.isPass) {
 			setLastStatus("PASS");

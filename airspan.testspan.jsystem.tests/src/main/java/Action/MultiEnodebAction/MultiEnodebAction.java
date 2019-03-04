86cb2a98780a86cb20c3c2940627642189675878
ggrunwald <ggrunwald@ASIL-GGRUNWALD.airspan.com>
2019-03-04 09:05:55 +0200

    Add pre test
    Former-commit-id: f50e02f04639131620b2181d469362d67ac840d5

--- airspan.testspan.jsystem.tests/src/main/java/Action/MultiEnodebAction/MultiEnodebAction.java
+++ airspan.testspan.jsystem.tests/src/main/java/Action/MultiEnodebAction/MultiEnodebAction.java
@@ -13,7 +13,9 @@ import EnodeB.EnodeB;
 import UE.UE;
 import Utils.CommonConstants;
 import Utils.GeneralUtils;
+import Utils.SetupUtils;
 import Utils.SysObjUtils;
+import jsystem.extensions.report.html.Report;
 import jsystem.framework.ParameterProperties;
 import jsystem.framework.TestProperties;
 import jsystem.framework.report.Reporter;
@@ -37,17 +39,23 @@ public class MultiEnodebAction extends EnodebAction {
 	public static HashMap<String, String> passCriteria = new HashMap<>();
 	static ArrayList<UE> dynUEList = new ArrayList<>();
 	ArrayList<UE> statUEList = new ArrayList<>();
-	public String succCounterName;
-
-	public String attCounterName;
-	public static String succCounterNameSNMP;
-	public static String attCounterNameSNMP;
+	//public String succCounterName;
+	//public String attCounterName;
+	//public static String succCounterNameSNMP;
+	//public static String attCounterNameSNMP;
 	public static EnodeB dut1;
-	public EnodeB dut2;
+	public static EnodeB dut2;
 	private static int attenuatorMin;
 	private static int attenuatorMax;
 	private EnodeBConfig enodeBConfig;
 
+	static String counterAttempt;
+	static String counterSuccess;
+	
+	static String counterAttemptsnmp;
+	static String counterSuccesssnmp;
+
+	
 	// results variables
 	public static int numberOfDynamicUES;
 	private static double HO_Per_Min_Per_UE;
@@ -62,22 +70,41 @@ public class MultiEnodebAction extends EnodebAction {
 	private static double total_HO_Success_rate_out_of_attempts;
 	private static double theoreticalAttempts;
 	
+	private static boolean startedHO = false;
+	private static boolean finishedHO = false;
+	private static int finalCounter = 0;
+	
+	
+	public static synchronized boolean isStartedHO() {
+		return startedHO;
+	}
+
+	public static synchronized void setStartedHO(boolean startedHO) {
+		MultiEnodebAction.startedHO = startedHO;
+	}
+
+	public static synchronized boolean isFinishedHO() {
+		return finishedHO;
+	}
+
+	public static synchronized void setFinishedHO(boolean finishedHO) {
+		MultiEnodebAction.finishedHO = finishedHO;
+	}
+
 	@Override
 	public void init() {
-		enbInTest = new ArrayList<EnodeB>();
-        
-        /*if (dut1 == null){
+        if (dut1 == null){
         	report.report("DUT1 is not loaded",Reporter.FAIL);
         	Assume.assumeTrue(false);        	
         }
 		if (dut2 == null){
 			report.report("DUT2 is not loaded",Reporter.FAIL);
 			Assume.assumeTrue(false);			
-		}*/
-		//enbInTest = new ArrayList<>();
-		//enbInTest.add(dut1);
-		//enbInTest.add(dut2);
-        //super.init();
+		}
+		enbInTest = new ArrayList<>();
+		enbInTest.add(dut1);
+		enbInTest.add(dut2);
+        super.init();
 		objectInit();
 	}
 	
@@ -89,14 +116,6 @@ public class MultiEnodebAction extends EnodebAction {
 	 */
 	public void objectInit() {
 		enodeBConfig = EnodeBConfig.getInstance();
-		enodeB = dut1;
-		neighbor = dut2;
-		peripheralsConfig = PeripheralsConfig.getInstance();
-		attenuatorSetUnderTest = AttenuatorSet.getAttenuatorSet(CommonConstants.ATTENUATOR_SET_NAME);
-		attenuatorMin = attenuatorSetUnderTest.getMinAttenuation();
-		attenuatorMax = attenuatorSetUnderTest.getMaxAttenuation();
-		ATT_STEP_TIME = attenuatorSetUnderTest.getStepTime();
-		ATT_STEP = attenuatorSetUnderTest.getAttenuationStep();
 	}
 
 	public FrequencyType freqType = FrequencyType.Intra;
@@ -139,48 +158,90 @@ public class MultiEnodebAction extends EnodebAction {
 	static Integer defaultTimeHO = 10;
 	
 	@Test
-    @TestProperties(name = "Start HO", returnParam = {"LastStatus"}, paramsInclude = {"TimeToHO"})
+    @TestProperties(name = "Get Statistics Hand Over", returnParam = {"LastStatus"}, paramsInclude = {})
+	public void getStatisticsHO() {
+		if(!isStartedHO()){
+			report.report("Hand Over was not started. No results", Reporter.FAIL);
+		}else if(isFinishedHO()){
+			LongHOResults();
+			setStartedHO(false);
+			setFinishedHO(false);
+		}else{
+			while(true){
+				report.report("Hand Over did not finish. Wait more 5 minutes");
+				GeneralUtils.unSafeSleep(5*60*1000);
+				if(isFinishedHO()){
+					LongHOResults();
+					setStartedHO(false);
+					setFinishedHO(false);
+					return;
+				}
+			}
+		}
+	}
+	
+	@Test
+    @TestProperties(name = "Start HO", returnParam = {"LastStatus"}, paramsInclude = {"DUT1","DUT2","TimeToHO",
+    		"FreqType","HoType"})
 	public void startHO() {
+		if(isStartedHO()){
+			report.report("HO has already been started and cannot be started again", Reporter.FAIL);
+			return;
+		}
 		if(timeToHO == null){
 			report.report("No time was set. Default of " + defaultTimeHO + " minutes");
 			timeToHO = defaultTimeHO;
 		}
+		
+		preTest();
+		
+		counterSuccess = "S1IntraFreqInCompSuccRnlRadioRsn";
+		counterSuccesssnmp = "asLteStatsHoS1IntraFreqInCompSuccRnlRadioRsn";
+		counterAttempt = "S1IntraFreqInPrepSuccRnlRadioRsn";
+		counterAttemptsnmp = "asLteStatsHoS1IntraFreqInPrepSuccRnlRadioRsn";
+
+		
 		/*counterAttempt = hoType.toString() + freqType + "FreqInCompSuccRnlRadioRsn";
 		counterSuccess = hoType.toString() + freqType + "FreqInAttRnlRadioRsn";;
 		
 		counterAttemptsnmp = "asLteStatsHo" + counterAttempt;
-		counterSuccesssnmp = "asLteStatsHo" + counterSuccess;
+		counterSuccesssnmp = "asLteStatsHo" + counterSuccess;*/
+				
+		if (!gettingPassCratiriaValue()) {
+			report.report("Failed to reset counters", Reporter.FAIL);
+		}
 		
-		ho = new P0();
-		ho.setDUT1(dut1.getNetspanName());
-		ho.setDUT2(dut2.getNetspanName());
-		ho.setPerformDbComperison(false);
-		ho.setInitCommands(false);
-		try {
-			ho.init();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}*/
+		/*attenuatorSetUnderTest = AttenuatorSet.getAttenuatorSet(CommonConstants.ATTENUATOR_SET_NAME);
+		attenuatorMin = attenuatorSetUnderTest.getMinAttenuation();
+		attenuatorMax = attenuatorSetUnderTest.getMaxAttenuation();
+		ATT_STEP_TIME = attenuatorSetUnderTest.getStepTime();
+		ATT_STEP = attenuatorSetUnderTest.getAttenuationStep();*/
 		
+		HandOverClass hoclass = new HandOverClass();
+		hoclass.start();
+		
+
+    }
+
+	private void preTest() {
+		dynUEList = SetupUtils.getInstance().getDynamicUEs();
+		enodeB = dut1;
+		neighbor = dut2;
+		peripheralsConfig = PeripheralsConfig.getInstance();
 		attenuatorSetUnderTest = AttenuatorSet.getAttenuatorSet(CommonConstants.ATTENUATOR_SET_NAME);
 		attenuatorMin = attenuatorSetUnderTest.getMinAttenuation();
 		attenuatorMax = attenuatorSetUnderTest.getMaxAttenuation();
 		ATT_STEP_TIME = attenuatorSetUnderTest.getStepTime();
 		ATT_STEP = attenuatorSetUnderTest.getAttenuationStep();
 		
-		HandOverClass hoclass = new HandOverClass();
-		hoclass.start();
-		
-	/*	succCounterName = "S1InterFreqInCompSuccRnlRadioRsn";
-		succCounterNameSNMP = "asLteStatsHoS1InterFreqInCompSuccRnlRadioRsn";
-		attCounterName = "S1InterFreqInPrepSuccRnlRadioRsn";
-		attCounterNameSNMP = "asLteStatsHoS1InterFreqInPrepSuccRnlRadioRsn";*/
-
-    }
+	}
 
 	static class HandOverClass implements Runnable{
 		public void start() {
+			GeneralUtils.startLevel("Moving attenuator for " + maxduration / 60000 + " minutes to create Hand-Over");
+			report.report("Attenuator IP: " + attenuatorSetUnderTest.getName());
+			GeneralUtils.stopLevel();
+			startedHO = true;
 			Thread runnableThread = new Thread(this);
 			runnableThread.start();
 		}
@@ -192,10 +253,7 @@ public class MultiEnodebAction extends EnodebAction {
 			long resetCounters = startTime;
 			int counter = 0;
 			maxduration = timeToHO*60*1000;
-			GeneralUtils.startLevel("Moving attenuator for " + maxduration / 60000 + " minutes to create Hand-Over");
-			report.report("Attenuator IP: " + attenuatorSetUnderTest.getName());
-			GeneralUtils.stopLevel();
-			//int waitingPeriodTime = dut1.getGranularityPeriod();
+			int waitingPeriodTime = dut1.getGranularityPeriod();
 			while (System.currentTimeMillis() < endTime) {
 				moveAtt(attenuatorSetUnderTest, attenuatorMin, attenuatorMax);
 				counter++;
@@ -204,23 +262,28 @@ public class MultiEnodebAction extends EnodebAction {
 				}
 				moveAtt(attenuatorSetUnderTest, attenuatorMax, attenuatorMin);
 				counter++;
-				/*if (System.currentTimeMillis() - resetCounters >= 8 * 60 * 1000 * waitingPeriodTime) {
+				if (System.currentTimeMillis() - resetCounters >= 8 * 60 * 1000 * waitingPeriodTime) {
 					resetCounters = System.currentTimeMillis();
 					double temp = Math.floor(((System.currentTimeMillis() - startTime) / 1000 / 60.0) * 100);
 					double tempDouble = temp / 100.0;
 					report.report("Time elapsed: " + tempDouble + " minutes.");
 					updateResultsVariables(counter);
 					resetCounters();
-				}*/
+				}
 			}
 			 // moving attenuator stop level
 			report.report("Finished moving attenuator");
+			report.report("Wait for " + waitingPeriodTime + " minutes for Counter to update");
+			GeneralUtils.unSafeSleep(waitingPeriodTime * 60 * 1000);
+			updateResultsVariables(counter);
+			setFinishedHO(true);
+			finalCounter = counter;
 		}
 	}
 
 
 
-	@Test
+	/*@Test
 	@TestProperties(name = "BasicHO_X2IntraFrequency", returnParam = { "IsTestWasSuccessful" }, paramsExclude = {
 			"IsTestWasSuccessful" })
 	public void BasicHO_X2IntraFrequency() {
@@ -261,7 +324,7 @@ public class MultiEnodebAction extends EnodebAction {
 		}
 		passCriteria.put(succCounterName, succCounterNameSNMP);
 		passCriteria.put(attCounterName, attCounterNameSNMP);
-	}
+	}*/
 
 	/* Basic HO - X2 Inter Frequency */
 	@Test
@@ -272,13 +335,7 @@ public class MultiEnodebAction extends EnodebAction {
 		passCriteria.put("HoX2InterFreqInCompSuccRnlRadioRsn", "HoX2InterFreqInCompSuccRnlRadioRsn");
 	}
 
-
-
-
-
-
-
-	@Test
+	/*@Test
 	@TestProperties(name = "MultipleHO_X2InterFrequency", returnParam = { "IsTestWasSuccessful" }, paramsExclude = {
 			"IsTestWasSuccessful" })
 	public void MultipleHO_X2InterFrequency() {
@@ -323,11 +380,6 @@ public class MultiEnodebAction extends EnodebAction {
 
 	}
 
-
-
-
-
-
 	private void setX2IntraCounters() {
 		if (enodeB.isInstanceOfXLP_15_2()) {
 			succCounterName = "HoX2IntraFreqInCompSuccRnlRadioRsn";
@@ -343,8 +395,6 @@ public class MultiEnodebAction extends EnodebAction {
 		passCriteria.put(succCounterName, succCounterNameSNMP);
 		passCriteria.put(attCounterName, attCounterNameSNMP);
 	}
-
-
 
 	@Test
 	@TestProperties(name = "MultipleHO_S1IntraFrequency", returnParam = { "IsTestWasSuccessful" }, paramsExclude = {
@@ -364,7 +414,7 @@ public class MultiEnodebAction extends EnodebAction {
 		}
 		passCriteria.put(succCounterName, succCounterNameSNMP);
 		passCriteria.put(attCounterName, attCounterNameSNMP);
-	}
+	}*/
 
 	public boolean HoLongTest() {
 		boolean flag = true;
@@ -410,15 +460,9 @@ public class MultiEnodebAction extends EnodebAction {
 		report.report("Wait for " + waitingPeriodTime + " minutes for Counter to update");
 		GeneralUtils.unSafeSleep(waitingPeriodTime * 60 * 1000);
 
-		flag = LongHOResults(flag, counter);
+		//flag = LongHOResults(flag, counter);
 		return flag;
 	}
-
-	
-
-	
-
-	
 
 	private boolean verifyCountersSingleHO(EnodeB enb) {
 		GeneralUtils.startLevel("Counters value for EnodeB " + enb.getNetspanName());
@@ -523,15 +567,14 @@ public class MultiEnodebAction extends EnodebAction {
 		}
 	}
 
-	private boolean LongHOResults(boolean flag, int counter) {
-		updateResultsVariables(counter);
+	private void LongHOResults() {
 
 		GeneralUtils.startLevel("Results");
 		report.report("Attenuator step: " + ATT_STEP);
 		report.report("Attenuator timing: " + ATT_STEP_TIME + "ms");
 		report.report("Attenuation boundaries: " + attenuatorMin + "-" + attenuatorMax);
 		report.report("Number of dynamic UE's: " + numberOfDynamicUES);
-		report.report("Number of expected HO's: " + counter * numberOfDynamicUES);
+		report.report("Number of expected HO's: " + finalCounter * numberOfDynamicUES);
 
 		GeneralUtils.startLevel("number of expected HO's per minute per UE: " + HO_Per_Min_Per_UE);
 		report.report("total Attenuator moves / test duration in minutes / number of dynamic UES");
@@ -539,20 +582,19 @@ public class MultiEnodebAction extends EnodebAction {
 									// level
 
 		GeneralUtils.startLevel("Number of attempts: " + total_attCounter);
-		report.report("SNMP MIB: " + attCounterNameSNMP + " On Both EnodeBs");
+		report.report("SNMP MIB: " + counterAttemptsnmp + " On Both EnodeBs");
 		report.report(enodeB.getNetspanName() + ": " + first_ENB_att);
 		report.report(neighbor.getNetspanName() + ": " + second_ENB_att);
 		GeneralUtils.stopLevel();// Number of attempts: stop level
 
 		GeneralUtils.startLevel("Number of HO's: " + total_succCounter);
-		report.report("SNMP MIB: " + succCounterNameSNMP + " On Both EnodeBs");
+		report.report("SNMP MIB: " + counterSuccesssnmp + " On Both EnodeBs");
 		report.report(enodeB.getNetspanName() + ": " + first_ENB_succ);
 		report.report(neighbor.getNetspanName() + ": " + second_ENB_succ);
 		GeneralUtils.stopLevel(); // Number of HO's: stop level
 
 		if (total_attCounter == 0) {
 			report.report("There were no attempts - test failed", Reporter.WARNING);
-			flag = false;
 		} else {
 			if (theoreticalAttempts < 60) {
 				GeneralUtils.startLevel("Number of attempts out of real attenuator moves is: "
@@ -560,7 +602,6 @@ public class MultiEnodebAction extends EnodebAction {
 				report.report("Number of attempts is less then 60%", Reporter.WARNING);
 				report.report("Number of attempts / attenuator moves");
 				GeneralUtils.stopLevel();
-				flag = false;
 			} else {
 				if (theoreticalAttempts < 85) {
 					GeneralUtils.startLevel("Number of attempts out of real attenuator moves is: "
@@ -577,13 +618,12 @@ public class MultiEnodebAction extends EnodebAction {
 
 				GeneralUtils.startLevel("Pass Criteria: Success rate (out of attempts): "
 						+ GeneralUtils.fractureToPercent(total_HO_Success_rate_out_of_attempts) + "%");
-				report.report("SNMP MIB: " + succCounterNameSNMP + " On Both EnodeBs / SNMP MIB: " + attCounterNameSNMP
+				report.report("SNMP MIB: " + counterSuccesssnmp + " On Both EnodeBs / SNMP MIB: " + counterAttemptsnmp
 						+ " On Both EnodeBs");
 				if (total_HO_Success_rate_out_of_attempts < WARNING_LEVEL) {
 					report.report(
 							"Success rate lower then Threshold (" + GeneralUtils.fractureToPercent(PASS_LEVEL) + ")",
 							Reporter.WARNING);
-					flag = false;
 				}
 				if (total_HO_Success_rate_out_of_attempts > WARNING_LEVEL
 						&& total_HO_Success_rate_out_of_attempts < PASS_LEVEL) {
@@ -600,19 +640,18 @@ public class MultiEnodebAction extends EnodebAction {
 			}
 		}
 		GeneralUtils.stopLevel(); // Results stop level
-		return flag;
 	}
 
 	private static void updateResultsVariables(int counter) {
 		numberOfDynamicUES = dynUEList.size();
 		HO_Per_Min_Per_UE = counter * 1.0 / GeneralUtils.milisToMinutes(maxduration) * 1.0;
 
-		first_ENB_succ += enodeB.getCountersValue(succCounterNameSNMP);
-		first_ENB_att += enodeB.getCountersValue(attCounterNameSNMP);
+		first_ENB_succ += enodeB.getCountersValue(counterSuccesssnmp);
+		first_ENB_att += enodeB.getCountersValue(counterAttemptsnmp);
 		firstENB_HO_Success_rate_out_of_attempts = (first_ENB_succ * 1.0) / (first_ENB_att * 1.0);
 
-		second_ENB_succ += neighbor.getCountersValue(succCounterNameSNMP);
-		second_ENB_att += neighbor.getCountersValue(attCounterNameSNMP);
+		second_ENB_succ += neighbor.getCountersValue(counterSuccesssnmp);
+		second_ENB_att += neighbor.getCountersValue(counterAttemptsnmp);
 		secondENB_HO_Success_rate_out_of_attempts = (second_ENB_succ * 1.0) / (second_ENB_att * 1.0);
 
 		total_succCounter = first_ENB_succ + second_ENB_succ;
@@ -625,14 +664,14 @@ public class MultiEnodebAction extends EnodebAction {
 	@ParameterProperties(description = "First DUT")
 	public void setDUT1(String dut) {
 		GeneralUtils.printToConsole("Load DUT1" + dut);
-		this.dut1 = (EnodeB) SysObjUtils.getInstnce().initSystemObject(EnodeB.class, false, dut).get(0);
+		MultiEnodebAction.dut1 = (EnodeB) SysObjUtils.getInstnce().initSystemObject(EnodeB.class, false, dut).get(0);
 		GeneralUtils.printToConsole("DUT loaded" + this.dut1.getNetspanName());
 	}
 
 	@ParameterProperties(description = "Second DUT")
 	public void setDUT2(String dut) {
 		GeneralUtils.printToConsole("Load DUT2" + dut);
-		this.dut2 = (EnodeB) SysObjUtils.getInstnce().initSystemObject(EnodeB.class, false, dut).get(0);
+		MultiEnodebAction.dut2 = (EnodeB) SysObjUtils.getInstnce().initSystemObject(EnodeB.class, false, dut).get(0);
 		GeneralUtils.printToConsole("DUT loaded" + this.dut2.getNetspanName());
 	}
 
@@ -646,7 +685,7 @@ public class MultiEnodebAction extends EnodebAction {
 
 	@Override
 	public void end() {
-		//super.end();
+		super.end();
 	}
 
 	private boolean gettingPassCratiriaValue() {

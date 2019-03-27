fca1784385f377cd70ed37408498049cf948a219
pzionit <pzionit@airspan.com>
2019-03-27 19:55:35 +0200

    AUT-1305 - Logs - added for debug.
    Former-commit-id: fa72a388782a0911981965ec5dc6fc366aa40022

--- airspan.testspan.jsystem.sysobj/src/main/java/EnodeB/Components/XLP/XLP.java
+++ airspan.testspan.jsystem.sysobj/src/main/java/EnodeB/Components/XLP/XLP.java
@@ -564,7 +564,7 @@ public abstract class XLP extends EnodeBComponent {
      */
     @Override
     public void setSessionLogLevel(String sessionName,String client, String process, int level) {
-        GeneralUtils.printToConsole("DEBUG: logger threshold set client=* process=* cli=" + String.valueOf(level));
+        GeneralUtils.printToConsole("**DEBUG: " +sessionName+" "+client+" "+process+" "+String.valueOf(level));
         sendCommandsOnSession(sessionName, LTE_CLI_PROMPT, String.format("logger threshold set client=%s process=%s cli=%s", client, process, String.valueOf(level)), LTE_CLI_PROMPT);
     }
 

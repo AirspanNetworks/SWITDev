3a1486a9806af0e0e8e618f59910ff60cfc29179
ggrunwald <ggrunwald@ASIL-GGRUNWALD.airspan.com>
2019-06-05 13:54:12 +0300

    fix json property

--- airspan.testspan.jsystem.sysobj/src/main/java/UeSimulator/Amarisoft/JsonObjects/ConfigFile/Channel.java
+++ airspan.testspan.jsystem.sysobj/src/main/java/UeSimulator/Amarisoft/JsonObjects/ConfigFile/Channel.java
@@ -13,12 +13,12 @@ public class Channel {
     @JsonProperty("type")
     private String type;
 
-    @JsonProperty("apn")
+    @JsonProperty("type")
     public String getType() {
         return type;
     }
 
-    @JsonProperty("apn")
+    @JsonProperty("type")
     public void setType(String type) {
         this.type = type;
     }

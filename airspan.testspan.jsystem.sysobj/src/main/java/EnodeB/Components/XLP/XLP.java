ec551121150ec75c9e449c5b1f2c0e18825b8997
Dor Shalom <dshalom@airspan.com>
2019-08-12 17:42:59 +0300

    Handle null in getMajorVersions

--- airspan.testspan.jsystem.sysobj/src/main/java/EnodeB/Components/XLP/XLP.java
+++ airspan.testspan.jsystem.sysobj/src/main/java/EnodeB/Components/XLP/XLP.java
@@ -265,6 +265,8 @@ public class XLP extends EnodeBComponent {
 				enodebStandbyVersion = getMajorVer(getStandbyVersion());
 				GeneralUtils.printToConsole(
 						"SNMP returned running ver: " + enodebRunningVersion + " , standby: " + enodebStandbyVersion);
+				if(enodebRunningVersion == null || enodebStandbyVersion == null)
+					return false;
 				return true;
 			}
 		} catch (IOException e) {
